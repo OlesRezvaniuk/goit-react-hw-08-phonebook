167d0e30ecbae18d6a759bac65b5f24a5d4bf500
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-11-06 16:31:07 +0200

    r
    r

--- src/redux/root.reduser.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { contactListReducer } from './PhonebookSlice';
-
-export const rootReducer = combineReducers({
-  contactList: contactListReducer,
-});
