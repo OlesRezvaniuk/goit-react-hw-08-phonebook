ee44008240812f1a61b3cf33dd316763462bb91d
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-11-08 09:56:30 +0200

    r
    r

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,8 +2,15 @@ import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { Contacts } from './Contacts/Contacts';
 import { PhoneBookBox } from './Phonebook.styled';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getContactsData } from 'redux/Phonebook/contactsOperations';
 
 export const Phonebook = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getContactsData());
+  }, [dispatch]);
   return (
     <PhoneBookBox>
       <Form />
