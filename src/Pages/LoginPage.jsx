870650004c42fd2d048d1ef24faeb4a26c557fa7
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-11-10 18:51:47 +0200

    r
    r

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import authOperations from 'redux/RepetaApi';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export const LoginPage = () => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const token = useSelector(state => state.auth.token);
 
   const handleEmailLogin = e => {
     setEmail(e.currentTarget.value);
@@ -21,36 +24,45 @@ export const LoginPage = () => {
   };
   return (
     <>
-      <h1>Log in to Phonebook</h1>
+      {token ? (
+        <p>
+          You are already sing in, <Link to="/contacts">to Contacts</Link>
+        </p>
+      ) : (
+        <>
+          <h1>Log in to Phonebook</h1>
 
-      <form onSubmit={handleLoginSubmit}>
-        <label>
-          <span>Email</span>
-          <input
-            type="email"
-            name="email"
-            placeholder="Your e-mail"
-            aria-label="Input for your email"
-            onChange={handleEmailLogin}
-            value={email}
-          />
-        </label>
+          <form onSubmit={handleLoginSubmit}>
+            <label>
+              <span>Email</span>
+              <input
+                type="email"
+                name="email"
+                placeholder="Your e-mail"
+                aria-label="Input for your email"
+                onChange={handleEmailLogin}
+                value={email}
+              />
+            </label>
 
-        <br />
-        <label>
-          <span>Password</span>
-          <input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            aria-label="Input for your password"
-            onChange={handlePasswordLogin}
-            value={password}
-          />
-        </label>
-        <br />
-        <button>Log in</button>
-      </form>
+            <br />
+            <label>
+              <span>Password</span>
+              <input
+                type="password"
+                name="password"
+                placeholder="Your password"
+                aria-label="Input for your password"
+                onChange={handlePasswordLogin}
+                value={password}
+              />
+            </label>
+            <br />
+            <button>Log in</button>
+          </form>
+        </>
+      )}
+      ;
     </>
   );
 };
--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import authOperations from 'redux/RepetaApi';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export const RegisterPage = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const token = useSelector(state => state.auth.token);
 
   const handleNameChange = e => {
     setName(e.currentTarget.value);
@@ -27,46 +30,55 @@ export const RegisterPage = () => {
 
   return (
     <>
-      <h1>Create your account</h1>
-      <form onSubmit={handleSubmit}>
-        <label name="name" type="name">
-          <span>Name</span>
-          <input
-            placeholder="Your name"
-            aria-label="Input for your name"
-            onChange={handleNameChange}
-            value={name}
-          />
-        </label>
-        <br />
-        <label>
-          <span>Email</span>
-          <input
-            name="email"
-            variant="standard"
-            placeholder="Your e-mail"
-            aria-label="Input for your email"
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </label>
-        <br />
-        <label>
-          <span>Password</span>
-          <input
-            name="password"
-            placeholder="Should be at least 7 characters"
-            aria-label="Input for your password"
-            type="password"
-            variant="standard"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </label>
-        <br />
-        <button>Sing Up</button>
-      </form>
+      {token ? (
+        <p>
+          You are already sing up, <Link to="/contacts">to Contacts</Link>
+        </p>
+      ) : (
+        <>
+          {' '}
+          <h1>Create your account</h1>
+          <form onSubmit={handleSubmit}>
+            <label name="name" type="name">
+              <span>Name</span>
+              <input
+                placeholder="Your name"
+                aria-label="Input for your name"
+                onChange={handleNameChange}
+                value={name}
+              />
+            </label>
+            <br />
+            <label>
+              <span>Email</span>
+              <input
+                name="email"
+                variant="standard"
+                placeholder="Your e-mail"
+                aria-label="Input for your email"
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </label>
+            <br />
+            <label>
+              <span>Password</span>
+              <input
+                name="password"
+                placeholder="Should be at least 7 characters"
+                aria-label="Input for your password"
+                type="password"
+                variant="standard"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </label>
+            <br />
+            <button>Sing Up</button>
+          </form>
+        </>
+      )}
     </>
   );
 };
